4fad9c48e89b31eb4297cf2d91d06b6f4b698875
Halcyon1902 <huynhgiahao1902@gmail.com>
2022-06-01 12:47:02 +0700

    lần 1

---
+++ Big_School/Controllers/CoursesController.cs
@@ -0,0 +1,24 @@
+﻿using Big_School.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Big_School.Controllers
+{
+    
+    public class CoursesController : Controller
+    {
+        private ApplicationDbContext _dbContext;
+        public CoursesController()
+        {
+            _dbContext = new ApplicationDbContext();
+        }
+        // GET: Courses
+        public ActionResult Create()
+        {
+            return View();
+        }
+    }
+}
---
+++ Big_School/Models/Course.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Big_School.Models
+{
+    public class Course
+    {
+        public int Id
+        {
+            get;set;
+        }
+        public ApplicationUser Lecturrer
+        {
+            get;set;
+        }
+        [Required]
+        public string LectureID
+        {
+            get;set;
+        }
+        [Required]
+        [StringLength(225)]
+        public string Place
+        {
+            get; set;
+        }
+        public DateTime DateTime
+        {
+            get; set;
+        }
+        public Category Category
+        {
+            get; set;
+        }
+        public byte CategoryId
+        {
+            get; set;
+        }
+
+    }
+   
+}
